e95069f4c8f75578620668b2951dd71f2417787e
Metehan Olgun <metehannolgun@gmail.com>
2025-04-17 23:50:17 +0300

    Data service eklendi

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { use } from 'react'
 
 const ProductsPage = () => {
+  const [loadedProducts, setLoadedProducts] = useState([])
+  useEffect(() => {
+    async function fetchProducts() {
+      const response = await fetch('http://localhost:5000/products');
+      const data = await response.json();
+      setLoadedProducts(data);
+    }
+    fetchProducts();
+  }, [])
   return (
     <h1>ProductsPage</h1>
   )
