2764b2b323ca1fda3e616d5beb2da0f2e2330dc1
Metehan Olgun <metehannolgun@gmail.com>
2025-05-02 16:45:27 +0300

    Card sayfası ve ürün ekleme işlemleri yapıldı

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,22 @@
-import { CardMedia, Card, CardContent, Typography, CardActions, IconButton, Button, CardActionArea } from '@mui/material'
+import { CardMedia, Card, CardContent, Typography, CardActions, IconButton, Button, CardActionArea, CircularProgress } from '@mui/material'
 import React from 'react'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import { Link } from 'react-router-dom'
 import { currenyTRY } from '../utils/format'
+import requests from '../api/apiClient'
+import { useState } from 'react'
 
 
 const ProductCard = ({product}) => {
+    const [loading, setLoading] = useState(false);
+    function handleAddItem(productId) {
+        setLoading(true);
+        requests.cart.addItem(productId,1)
+            .then(cart => console.log(cart))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
+        }
+
   return (
     <Card>
         <CardActionArea component={Link} to={'/products/' + product.id}>
@@ -33,7 +44,9 @@ const ProductCard = ({product}) => {
             <IconButton>
                 <FavoriteBorderIcon/>
             </IconButton>
-            <Button>Sepete Ekle</Button>
+            <Button onClick={() => handleAddItem(product.id)}>
+                {loading ? <CircularProgress size="20px" /> : 'Add to Cart'}
+            </Button>
         </CardActions>
     </Card>
   )
